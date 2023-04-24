db5f1b0ae2dab298352e92c5a381a451ea4ce1fb
Rohan1079 <114193371+Rohan1079@users.noreply.github.com>
2023-04-24 12:07:18 +1000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,90 @@
+<?php
+class user 
+{
+    public $name;
+    public $eMail;
+    public $userIssue;
+    public $comment;
+
+    function __construct()
+    {
+        $name = "";
+        $eMail = "";
+        $userIssue = "";
+        $comment = "";
+    }
+}
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+
+// define variables and set to empty values
+$nameErr = $emailErr = "";
+$name = $email = $comment  = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["userName"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["userName"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed";
+    }
+  }
+
+  if (empty($_POST["userEmail"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["userEmail"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format";
+    }
+  }
+
+  if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["comment"]);
+  }
+}
+?>
+<html>
+    <head>
+        <title> Contact Form </title>
+        <link rel ="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <h1> Contact Form </h1>
+        <hr>
+        <div class = "contactForm">
+        <form action="output.php" method = "post">
+            <label for = "name">Name:</label>
+            <input type="text" placeholder ="Your Name" name = "userName" required>
+            <br><br>
+            <label for = "userEmail">eMail:</label>
+            <input type = "text" placeholder ="Your eMail" name = "userEmail" required>
+            <br><br>
+            <label for = "userIssue" >Issue:</label>
+            <select name = "userIssue" required>
+                <option value = "Select"> Select your issue </option>
+                <option value = "Query"> Query </option>
+                <option value = "Feedback"> Feedback </option>
+                <option value = "Complaint"> Complaint </option>
+                <option value = "Other"> Other </option>
+            </select>
+            <br><br>
+            <lable for = "comment">Comment:</lable>
+            <textarea name="comment" rows = "5" cols ="40" required></textarea>
+            <br><br>
+            <input type = "submit" name = "submit" value = "Submit">
+            <hr>
+        </form>
+        </div>
+    </body>
+</html>
---
+++ output.php
@@ -0,0 +1,62 @@
+<?php
+include_once("index.php");
+
+$newUser = new user();
+$newUser->name = $_POST["userName"];
+$newUser->eMail = $_POST["userEmail"];
+$newUser->userIssue = $_POST["userIssue"];
+$newUser->comment = $_POST["comment"];
+
+function sqlEntry()
+        {
+            $servername = "localhost";
+            $username = "rohan";
+            $password = "Rohan@1079";
+            $dbname ="users";
+            $newUser = new user();
+            $newUser->name = $_POST["userName"];
+            $newUser->eMail = $_POST["userEmail"];
+            $newUser->userIssue = $_POST["userIssue"];
+            $newUser->comment = $_POST["comment"];
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        
+        if ($conn->connect_error)
+        {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        
+        echo "Connected successfully.";
+        
+        $stmt = $conn->prepare("INSERT INTO information (Name, eMail, Issue, comment) VALUES (?,?,?,?)");
+        $stmt->bind_param("ssss",$newUser->name, $newUser->eMail, $newUser->userIssue, $newUser->comment);
+        $stmt->execute();
+        $stmt->close();
+        $conn->close();
+
+        echo "Information is saved.";
+        
+        } 
+
+?>
+
+<html>
+    <head>
+    </head>
+    <body>
+        <h1>Your Information:</h1>
+        <hr>
+        <?php
+        ob_clean();
+        echo "Name : $newUser->name <br>";
+        echo "eMail : $newUser->eMail <br>";
+        echo "Issue : $newUser->userIssue <br>";
+        echo "Comment : $newUser->comment<br>";
+        ?>
+        <p> Incorrect Detalis? Press Edit. <br> Happy with the information, Save it! </p>
+        
+        <input type ="button" value="Edit" onclick ="history.back()">
+        <button onclick="<?php sqlEntry() ?>;"> Save It! </button>
+
+    </body>
+</html>
